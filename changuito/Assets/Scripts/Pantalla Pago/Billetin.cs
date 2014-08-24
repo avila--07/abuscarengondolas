add6d6a727a71672f511b928127a795594b7f851
navila <avila--07@hotmail.com>
2014-08-23 23:02:45 -0300

    commit

---
+++ changuito/Assets/Scripts/Pantalla Pago/Billetin.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billetin : MonoBehaviour {
+	private int clicks;
+	private Rect label = new Rect(0,0,Screen.width, Screen.height);
+    private GUIStyle style = new GUIStyle();
+
+	// Use this for initialization
+	void Start () {
+		clicks = 0;
+		style.fontSize = 32;
+		style.normal.textColor = Color.red;
+		style.fontStyle = FontStyle.Bold;
+
+        GUI.Button(new Rect(0, 0, 10, 10), "Suma " + clicks.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GUI.Label(label,"Suma "+ clicks.ToString() , style);
+        //GUI.Button(new Rect(0, 0, 10, 10), "Suma " + clicks.ToString());
+
+    }
+
+	void OnMouseDown(){
+		this.clicks++;
+  
+        //GUI.Label(label, "Suma " + clicks.ToString(), style);
+        print("Ya hiciste un click");
+
+    }
+
+
+}
+    
