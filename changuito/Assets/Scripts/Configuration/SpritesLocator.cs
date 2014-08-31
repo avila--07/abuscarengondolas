27bd8ad3a511f8491fa84a8624b1d0746a2ee8d0
Fernando Costantini <fhcostantini@gmail.com>
2014-09-01 00:13:07 +0100

    Posiciones relativas para encontrar los sprites

--- changuito/Assets/Scripts/Configuration/SpritesLocator.cs
+++ changuito/Assets/Scripts/Configuration/SpritesLocator.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public static class SpritesLocator
 {
-	public static string CHANGUITO_SPRITE = @"file://F:\Fer\Programacion\Fuentes\ABuscarEnGondolas\changuito\Assets\Sprites\Player.png";
-	public static string GONDOLA_SPRITE = @"file://F:\Fer\Programacion\Fuentes\ABuscarEnGondolas\changuito\Assets\Sprites\Gondola.png";
+	public static string CHANGUITO_SPRITE = @"file://.\Assets\Sprites\Player.png";
+	public static string GONDOLA_SPRITE = @"file://.\Assets\Sprites\Gondola.png";
 }
