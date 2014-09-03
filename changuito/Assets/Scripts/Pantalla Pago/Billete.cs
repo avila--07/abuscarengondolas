06036ce524bc1071c7e5157f968fc9cdfdbea4bf
cheppak <cheppi74@gmail.com>
2014-09-02 21:53:46 -0300

    Pantalla de Pago - Demo 1

---
+++ changuito/Assets/Scripts/Pantalla Pago/Billete.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billete : MonoBehaviour {
+
+    public int valor;
+	
+    public void OnClick()
+    {
+        
+        changeMontoIfPossible();
+    }
+
+
+    private void changeMontoIfPossible()
+    {
+        // mayor a 2 dado que es el objeto de menor valor, todavia podria seguir eligiendo
+        int nuevoResto =  getIntValueOfLabel("Resto") - this.valor; 
+        
+        if (nuevoResto > 2)
+        {
+            this.setIntValueOf("Sustraendo",-this.valor);
+            this.setIntValueOf("Resto", nuevoResto);
+            setTextLabelOf("GameMessageLabel", "¡Segui pagando tus productos!");
+        }
+        else
+        {
+            this.setIntValueOf("Resto", 0);
+            setTextLabelOf("GameMessageLabel","¡Muy bien! ¡Has pagado tus productos!");
+        }
+     }
+
+    //TODO: Mover!
+    private void setIntValueOf(string gameObjectName,int valor)
+    {
+        GameObject aGameObject = GameObject.Find(gameObjectName);
+        aGameObject.GetComponent<UILabel>().text = valor.ToString();
+    }
+
+    private int getIntValueOfLabel(string labalObject)
+    {
+        GameObject montoAcumulado = GameObject.Find(labalObject);
+        return int.Parse(montoAcumulado.GetComponent<UILabel>().text);
+    }
+   
+    private void setTextLabelOf(string objectLabel, string text)
+    {
+        GameObject ob = GameObject.Find(objectLabel);
+        ob.GetComponent<UILabel>().text = text;
+    }
+}
--- changuito/Assets/Scripts/Pantalla Pago/ServicioPago.cs
+++ changuito/Assets/Scripts/Pantalla Pago/ServicioPago.cs
@@ -1,15 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class ServicioPago : MonoBehaviour {
 
-	// Use this for initialization
+    int total = 75;
+    List<GameObject> moneyObjects = new List<GameObject>(6);
+    //REVIEW: Esta lista debe generarse en posiciones distintas?
+    // Asumimos que por ahora quedan fijas.
+    ArrayList moneyInSuperiorScene = new ArrayList(3);
+    ArrayList moneyInInferiorScene = new ArrayList(3);
+    // Use this for initialization
 	void Start () {
-	
+
+        this.calculateAndLoadMoney();
+        this.initializeScreen();
+        this.initializeMontoOnScreen();
+        this.InitializePizarra();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void InitializePizarra()
+    {
+        GameObject minuendo = GameObject.Find("Minuendo");
+        
+        GameObject resto = GameObject.Find("Resto");
+
+        minuendo.GetComponent<UILabel>().text = total.ToString();
+        resto.GetComponent<UILabel>().text = total.ToString();
+
+    }
+
+
+
+
+    //REVIEW: La cantidad de plata por pantalla viene definida por el monto? es dinamico?
+    //En tal caso ,tiene que ir a una fabrica o helper.
+    private void  calculateAndLoadMoney()
+    {
+        moneyInSuperiorScene.Add("Billete2");
+        moneyInSuperiorScene.Add("Billete5");
+        moneyInSuperiorScene.Add("Billete10");
+        moneyInInferiorScene.Add("Billete20");
+        moneyInInferiorScene.Add("Billete50");
+        moneyInInferiorScene.Add("Billete100");
+    }
+
+    private void initializeScreen()
+    {
+       
+      //Inicializamos en 2 grids distintas, cada una contiene 3 billetes.
+        GameObject gridSuperior = GameObject.Find("PagoBilletesGridSuperior");
+        GameObject gridInferior = GameObject.Find("PagoBilletesGridInferior");
+        
+        this.initializeGrid(gridSuperior, moneyInSuperiorScene);
+        this.initializeGrid(gridInferior, moneyInInferiorScene);
+    }
+    
+    private void initializeGrid(GameObject grid,ArrayList money)
+    {
+        foreach (string product in money)
+        {
+            GameObject loadedPrefab = Resources.Load<GameObject>(product);
+            NGUITools.AddChild(grid, loadedPrefab);
+        }
+        grid.GetComponent<UIGrid>().Reposition();
+
+    }
+
+
+    private void initializeMontoOnScreen()
+    {
+        GameObject text = GameObject.Find("MontoTotal");
+        text.GetComponent<UILabel>().text = total.ToString() + " Pesos";
+
+    }
+
+
 }
