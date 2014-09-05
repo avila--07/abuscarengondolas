8f29d52af579357c4bebeda5e72e51aab354c38f
cheppak <cheppi74@gmail.com>
2014-09-05 02:27:32 -0300

    Pago exacto

--- changuito/Assets/Scripts/Pantalla Pago/Billete.cs
+++ changuito/Assets/Scripts/Pantalla Pago/Billete.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Billete : MonoBehaviour {
+public class Billete : MonoBehaviour
+{
 
     public int valor;
-	
+
     public void OnClick()
     {
         changeMontoIfPossible();
@@ -13,22 +14,29 @@ public class Billete : MonoBehaviour {
     private void changeMontoIfPossible()
     {
         // mayor a 2 dado que es el objeto de menor valor, todavia podria seguir eligiendo
-        int nuevoResto = NGUISomosUtils.getIntFromGameObject("Resto") - this.valor; 
-        
+        int nuevoResto = NGUISomosUtils.getIntFromGameObject("Resto") - this.valor;
+
         if (nuevoResto > 2)
         {
-            NGUISomosUtils.showTextInScreen("Sustraendo",  "-" + this.valor.ToString());
+            NGUISomosUtils.showTextInScreen("Sustraendo", "-" + this.valor.ToString());
             NGUISomosUtils.showTextInScreen("Resto", nuevoResto.ToString());
             NGUISomosUtils.showTextInScreen("GameMessageLabel", "¡Segui pagando tus productos!");
         }
-        else
+        else if (nuevoResto < 2 && nuevoResto >= 0)
         {
             NGUISomosUtils.showTextInScreen("Resto", "0");
-            PagoStatus.pago = nuevoResto*-1;
+            PagoStatus.pago = PagoStatus.monto - nuevoResto;
             NGUISomosUtils.showTextInScreen("GameMessageLabel", "¡Muy bien! ¡Has pagado tus productos!");
             System.Threading.Thread.Sleep(250);
             Application.LoadLevel("PantallaControlVuelto");
         }
-     }
-         
+        if (nuevoResto < 0)
+        {
+            NGUISomosUtils.showTextInScreen("Resto", PagoStatus.monto.ToString());
+            NGUISomosUtils.showTextInScreen("Sustraendo", "-");
+            NGUISomosUtils.showTextInScreen("Minuendo", PagoStatus.monto.ToString());
+            PagoStatus.pago = 0;
+            NGUISomosUtils.showTextInScreen("GameMessageLabel", "¡Pagaste de mas! ¡Volve a intentarlo!");
+        }
+    }
 }
