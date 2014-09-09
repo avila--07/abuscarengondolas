842620630bf6b398745b689edd2fca5e56817347
cheppak <cheppi74@gmail.com>
2014-09-09 01:25:19 -0300

    Restart ante una nueva partida.

---
+++ changuito/Assets/Scripts/Configuration/RestartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Reinicio las variables que se van modificando durante el juego 
+/// antes de iniciar una nueva partida.
+/// </summary>
+public class RestartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        PagoStatus.monto = 0;
+        PagoStatus.pago = 0;
+	}
+}
