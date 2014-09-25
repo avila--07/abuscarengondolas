9eca1521ada330b66aa654d5835da4e5503379dd
cheppak <cheppi74@gmail.com>
2014-09-25 00:33:58 -0300

    Ventana Asignar jugador

---
+++ changuito/Assets/Scripts/Pantalla Configuracion/AsignarJugador.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AsignarJugador : MonoBehaviour {
+
+    public static Boolean isWindowsActive;
+
+    public void OnClick()
+    {
+        if (!isWindowsActive) { 
+            ConfigurationWindowsManager.instance.window1.SetActive(true);
+            ConfigurationWindowsManager.instance.window2.SetActive(false);
+            isWindowsActive = true;
+        }
+        else
+        {
+            ConfigurationWindowsManager.instance.window1.SetActive(false);
+            ConfigurationWindowsManager.instance.window2.SetActive(true);
+            isWindowsActive = false;
+        }
+    }
+}
---
+++ changuito/Assets/Scripts/Pantalla Configuracion/ConfigurationWindowsManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigurationWindowsManager : MonoBehaviour {
+
+    public GameObject window1;
+    public GameObject window2;
+
+    public static ConfigurationWindowsManager instance = null;
+   
+    void Awake() {
+        if (instance != null) 
+            Debug.LogError("GameManager must be attached to one gameobject only!");
+        instance = this;
+    }
+}
