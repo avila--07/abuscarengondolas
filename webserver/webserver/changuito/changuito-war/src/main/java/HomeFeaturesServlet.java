382304e0827ec8d572c56454b28bc5a7bdb7235c
Nicolas Avila <avila.nicolas@mercadolibre.com>
2014-09-26 02:15:40 -0300

    Agrego varios archivos para levantar las descripción del proyecto

---
+++ webserver/webserver/changuito/changuito-war/src/main/java/HomeFeaturesServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeFeaturesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+@Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+
+	try {
+		req.getRequestDispatcher("/features.html").forward(req, resp);
+	} catch (ServletException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	}
+}
