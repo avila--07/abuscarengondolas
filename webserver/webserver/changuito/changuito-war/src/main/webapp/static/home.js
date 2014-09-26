a7b80dabe5055ee50a9a8110720666152c95aaf4
Nicolas Avila <avila.nicolas@mercadolibre.com>
2014-09-26 02:13:29 -0300

    Agrego el link para volver al inicio

--- webserver/webserver/changuito/changuito-war/src/main/webapp/static/home.js
+++ webserver/webserver/changuito/changuito-war/src/main/webapp/static/home.js
@@ -4,5 +4,6 @@ $("#stats").on("click", function(){ $("#featured-wrapper").load("/statics") ;});
 $("#configuration").on("click", function(){ $("#featured-wrapper").load("/configuration"); });
 $("#partidas").on("click", function(){ $("#featured-wrapper").load("/partidas"); });
 $("#login").on("click", function(){ $("#featured-wrapper").load("/login") ;});
+$("#inicio").on("click", function(){ $("#featured-wrapper").load("/homefatures") ;});
 
 });
