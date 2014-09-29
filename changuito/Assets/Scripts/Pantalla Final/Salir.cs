109f30222909a754bc1cec6fdcd5a6cf8e71e644
cheppak <cheppi74@gmail.com>
2014-09-29 00:02:53 -0300

    Quit - Pantalla Final

---
+++ changuito/Assets/Scripts/Pantalla Final/Salir.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Salir : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+           Application.Quit();
+        }
+	}
+}
