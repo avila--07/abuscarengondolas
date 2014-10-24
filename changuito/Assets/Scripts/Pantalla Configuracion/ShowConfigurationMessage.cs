9e0f71fa1b67aaf893824e32824881864d116957
Cheppak <cheppi74@gmail.com>
2014-10-24 18:30:01 -0300

    Tuneamos un poco el boton para ingresar la configuracion. Ahora desparece si la espera es mayor a 5seg (ademas de que el contador vuelve a 0).

--- changuito/Assets/Scripts/Pantalla Configuracion/ShowConfigurationMessage.cs
+++ changuito/Assets/Scripts/Pantalla Configuracion/ShowConfigurationMessage.cs
@@ -1,23 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ShowConfigurationMessage : MonoBehaviour {
 
-    GameObject tooltip;
-	// Use this for initialization
-	void Start ()
+   
+    private int intentos;
+    private const int MAX_INTENTOS = 3;
+    DateTime lastTime;
+    public GameObject tooltip;
+    private const String dosIntentos = "Presione 2 veces para ingresar a la configuración";
+    private const String unIntento = "Presione 1 vez mas para ingresar a la configuración";
+    private const int MAX_WAIT_SECONDS = 5;
+
+    void Start ()
     {
-        tooltip = GameObject.Find("ConfigTooltipLabel");
-        tooltip.GetComponent<UILabel>().enabled = false;
+        this.intentos = 0;
+        this.tooltip.GetComponent<UILabel>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        TimeSpan span = DateTime.Now.Subtract(lastTime);
+        if (span.Seconds > MAX_WAIT_SECONDS)
+        {
+            this.intentos = 0;
+            this.tooltip.GetComponent<UILabel>().enabled = false;
+        }
 	}
 
     void OnClick()
     {
-        tooltip.GetComponent<UILabel>().enabled = true;
+        this.tooltip.GetComponent<UILabel>().enabled = true;
+        lastTime = DateTime.Now;
+        intentos++;
+        if (intentos == MAX_INTENTOS)
+        {
+            Application.LoadLevel("PantallaConfiguracion");
+        }
+        else if (this.intentos == 1)
+        {
+            this.tooltip.GetComponent<UILabel>().text = dosIntentos;
+        }
+        else if (this.intentos == 2)
+        {
+            this.tooltip.GetComponent<UILabel>().text = unIntento;
+        }
     }
 }
