3b5cdc4ae181c0987665b788910e607f646ca8f8
Fernando Costantini <fhcostantini@gmail.com>
2014-11-14 03:15:47 +0000

    t push origin dev Se guarda el id de la ultima partida

--- webserver/webserver/changuito/changuito-war/src/main/java/ar/com/utn/changuito/services/UploadStatisticsService.java
+++ webserver/webserver/changuito/changuito-war/src/main/java/ar/com/utn/changuito/services/UploadStatisticsService.java
@@ -1,8 +1,10 @@
 package ar.com.utn.changuito.services;
 
 import ar.com.utn.changuito.architecture.net.SharedObject;
+import ar.com.utn.changuito.model.game.User;
 import ar.com.utn.changuito.model.statistics.Statistic;
 import ar.com.utn.changuito.persistence.StatisticDAO;
+import ar.com.utn.changuito.persistence.UserDAO;
 
 public final class UploadStatisticsService extends SecuredService<Statistic> {
 
@@ -14,6 +16,18 @@ public final class UploadStatisticsService extends SecuredService<Statistic> {
     @Override
     protected SharedObject securedTypedCall(final Statistic statistic) {
 
+        final User authenticatedServiceUser = getAuthenticatedServiceUser();
+
+        long lastIdPartida = authenticatedServiceUser.getLastIdPartida();
+
+        // si es la primera estadistica
+        if ("fin_gondolas".equals(statistic.getIdEvento())) {
+            lastIdPartida++;
+            authenticatedServiceUser.setLastidPartidad(lastIdPartida);
+            new UserDAO().persist(authenticatedServiceUser);
+        }
+
+        statistic.setIdPartida(lastIdPartida);
         new StatisticDAO().persist(statistic);
         return null;
     }
