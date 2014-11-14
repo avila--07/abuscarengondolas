a8c1087a5ba295d199c22919c42dac3b396dbe27
Fernando Costantini <fhcostantini@gmail.com>
2014-11-14 03:15:47 +0000

    Se guarda el id de la ultima partida

--- webserver/webserver/changuito/changuito-war/src/main/java/ar/com/utn/changuito/services/UploadStatisticsService.java
+++ webserver/webserver/changuito/changuito-war/src/main/java/ar/com/utn/changuito/services/UploadStatisticsService.java
@@ -1,8 +1,10 @@
 package ar.com.utn.changuito.services;
 
 import ar.com.utn.changuito.architecture.net.SharedObject;
+import ar.com.utn.changuito.model.game.User;
 import ar.com.utn.changuito.model.statistics.Statistic;
 import ar.com.utn.changuito.persistence.StatisticDAO;
+import ar.com.utn.changuito.persistence.UserDAO;
 
 public final class UploadStatisticsService extends SecuredService<Statistic> {
 
@@ -14,7 +16,14 @@ public final class UploadStatisticsService extends SecuredService<Statistic> {
     @Override
     protected SharedObject securedTypedCall(final Statistic statistic) {
 
+        final User authenticatedServiceUser = getAuthenticatedServiceUser();
+
+        final long lastIdPartida = authenticatedServiceUser.getLastIdPartida();
+        authenticatedServiceUser.setLastidPartidad(lastIdPartida + 1);
+
+        statistic.setIdPartida(lastIdPartida);
         new StatisticDAO().persist(statistic);
+        new UserDAO().persist(authenticatedServiceUser);
         return null;
     }
 }
